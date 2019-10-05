18c730e4d2f9a3f1cefcc7b006a2494104815fa9
3xp1o1t <mrlol404@protonmail.com>
2019-10-05 18:22:55 -0500

    Integracion de vue-router al proyecto
    Desde ahora se controlan las opciones de menu
    con vue-router.

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,14 +7,19 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+// Vue Router
+import VueRouter from 'vue-router';
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+Vue.use(VueRouter);
+
+const routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/profile', component: require('./components/Profile.vue').default }
+]
+
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
@@ -29,4 +34,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
